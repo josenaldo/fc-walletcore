99ce06212194083795d66ac4e8162668ea006cce
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2024-07-28 16:32:10 -0300

    feat: add create client use case

---
+++ internal/usecase/create_client/create_client.go
@@ -0,0 +1,53 @@
+package createclient
+
+import (
+	"time"
+
+	"github.com/josenaldo/fc-walletcore/internal/entity"
+	"github.com/josenaldo/fc-walletcore/internal/gateway"
+)
+
+type CreateClientInputDto struct {
+	Name  string
+	Email string
+}
+
+type CreateClientOutputDto struct {
+	ID        string
+	CreatedAt time.Time
+	UpdateAt  time.Time
+	Name      string
+	Email     string
+}
+
+type CreateClientUseCase struct {
+	ClientGateway gateway.ClientGateway
+}
+
+func NewCreateClientUseCase(clientGateway gateway.ClientGateway) *CreateClientUseCase {
+	return &CreateClientUseCase{
+		ClientGateway: clientGateway,
+	}
+}
+
+func (usecase *CreateClientUseCase) Execute(input *CreateClientInputDto) (*CreateClientOutputDto, error) {
+	
+	
+	client, err := entity.NewClient(input.Name, input.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	err = usecase.ClientGateway.Save(client)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateClientOutputDto{
+		ID:        client.ID,
+		CreatedAt: client.CreatedAt,
+		UpdateAt:  client.UpdatedAt,
+		Name:      client.Name,
+		Email:     client.Email,
+	}, nil
+}
---
+++ internal/usecase/create_client/create_client_unit_test.go
@@ -0,0 +1,47 @@
+package createclient
+
+import (
+	"testing"
+
+	"github.com/josenaldo/fc-walletcore/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ClientGatewayMock struct {
+	mock.Mock
+}
+
+func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Client), args.Error(1)
+}
+
+func (m *ClientGatewayMock) Save(client *entity.Client) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+func TestCreateClientUseCaseExecute(t *testing.T) {
+	// Arrange - Given
+	m := &ClientGatewayMock{}
+	m.On("Save", mock.Anything).Return(nil)
+
+	uc := NewCreateClientUseCase(m)
+	intput := &CreateClientInputDto{
+		Name:  "Zé Galinha",
+		Email: "ze@galinha.com",
+	}
+
+	// Act - When
+	output, err := uc.Execute(intput)
+
+	// Assert - Then
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.NotEmpty(t, output.ID)
+	assert.Equal(t, intput.Name, output.Name)
+	assert.Equal(t, intput.Email, output.Email)
+	m.AssertExpectations(t)
+	m.AssertNumberOfCalls(t, "Save", 1)
+}
