b953edfd7de11a31f4fd192b05370e50ce570be4
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2024-11-08 13:18:17 -0300

    feat: Criação do projeto Balance-app

---
+++ balance-app/src/main/java/br/com/josenaldo/wbu/FcWalletcoreBalanceUpdaterApplication.java
@@ -0,0 +1,13 @@
+package br.com.josenaldo.wbu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcWalletcoreBalanceUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcWalletcoreBalanceUpdaterApplication.class, args);
+	}
+
+}
---
+++ balance-app/src/test/java/br/com/josenaldo/wbu/FcWalletcoreBalanceUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josenaldo.wbu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcWalletcoreBalanceUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
